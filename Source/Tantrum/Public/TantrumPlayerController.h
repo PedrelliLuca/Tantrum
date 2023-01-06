d6d56cf9242b84310a1d3d04b292cfaa54c588ec
Luca Pedrelli <luca.p@zuru.tech>
2023-01-06 17:20:31 +0100

    Creation of first C++ and BP classes

---
+++ Source/Tantrum/Public/TantrumPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TantrumPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANTRUM_API ATantrumPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
