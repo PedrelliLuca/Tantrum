c196a56a3cdf63a70443838056cfca987af82b2b
Luca Pedrelli <luca.p@zuru.tech>
2023-01-08 16:10:00 +0100

    Character camera

--- Source/Tantrum/Private/TantrumCharacterBase.cpp
+++ Source/Tantrum/Private/TantrumCharacterBase.cpp
@@ -3,3 +3,36 @@
 
 #include "TantrumCharacterBase.h"
 
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+ATantrumCharacterBase::ATantrumCharacterBase() {
+	GetCapsuleComponent()->InitCapsuleSize(42.f, 96.0f);
+		
+	// Rotate when the controller rotates.
+	bUseControllerRotationPitch = true;
+	bUseControllerRotationYaw = true;
+	bUseControllerRotationRoll = true;
+
+	// Configure character movement
+	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
+	
+	GetCharacterMovement()->JumpZVelocity = 700.f;
+	GetCharacterMovement()->AirControl = 0.35f;
+	GetCharacterMovement()->MaxWalkSpeed = 500.f;
+	GetCharacterMovement()->MinAnalogWalkSpeed = 20.f;
+	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
+
+	// Camera boom creation (pulls in towards the player if there is a collision)
+	_cameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	_cameraBoom->SetupAttachment(RootComponent);
+	_cameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character	
+	_cameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+
+	// Camera creation
+	_followCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	_followCamera->SetupAttachment(_cameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
+	_followCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+
+}
--- Source/Tantrum/Public/TantrumCharacterBase.h
+++ Source/Tantrum/Public/TantrumCharacterBase.h
@@ -3,12 +3,26 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
 
 #include "GameFramework/Character.h"
+#include "GameFramework/SpringArmComponent.h"
 
 #include "TantrumCharacterBase.generated.h"
 
 UCLASS()
 class TANTRUM_API ATantrumCharacterBase : public ACharacter {
 	GENERATED_BODY()
+
+public:
+	ATantrumCharacterBase();
+
+protected:
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	TObjectPtr<USpringArmComponent> _cameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	TObjectPtr<UCameraComponent> _followCamera;
 };
