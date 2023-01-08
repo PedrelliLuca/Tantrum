a3e1516387d0d3e7755dd34a97b927b75babcb70
Luca Pedrelli <luca.p@zuru.tech>
2023-01-08 15:19:08 +0100

    Input bindings moved to controller

--- Source/Tantrum/Public/TantrumCharacterBase.h
+++ Source/Tantrum/Public/TantrumCharacterBase.h
@@ -5,41 +5,10 @@
 #include "CoreMinimal.h"
 
 #include "GameFramework/Character.h"
-#include "InputMappingContext.h"
 
 #include "TantrumCharacterBase.generated.h"
 
 UCLASS()
-class TANTRUM_API ATantrumCharacterBase : public ACharacter
-{
+class TANTRUM_API ATantrumCharacterBase : public ACharacter {
 	GENERATED_BODY()
-
-public:
-	ATantrumCharacterBase();
-
-protected:
-	void BeginPlay() override;
-	
-	void SetupPlayerInputComponent(class UInputComponent* playerInputComponent) override;
-
-	/** MappingContext */
-	UPROPERTY(EditAnywhere, Category=Input)
-	TObjectPtr<UInputMappingContext> _defaultMappingContext;
-
-	/** Jump Input Action */
-	UPROPERTY(EditAnywhere, Category=Input)
-	TObjectPtr<UInputAction> _jumpAction;
-
-	/** Move Input Action */
-	UPROPERTY(EditAnywhere, Category=Input)
-	TObjectPtr<UInputAction> _moveAction;
-
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
-	TObjectPtr<UInputAction> _lookAction;
-
-private:
-	void _move(const FInputActionValue& value);
-
-	void _look(const FInputActionValue& value);
 };
--- Source/Tantrum/Public/TantrumPlayerController.h
+++ Source/Tantrum/Public/TantrumPlayerController.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 
 #include "GameFramework/PlayerController.h"
+#include "InputMappingContext.h"
 
 #include "TantrumPlayerController.generated.h"
 
@@ -15,11 +16,32 @@ UCLASS()
 class TANTRUM_API ATantrumPlayerController : public APlayerController {
 	GENERATED_BODY()
 
-
 public:
-
+	ATantrumPlayerController();
+	
 protected:
+	void BeginPlay() override;
 	
+	void SetupInputComponent() override;
+
+	UPROPERTY(EditAnywhere, Category=Input)
+	TObjectPtr<UInputMappingContext> _defaultMappingContext;
+
+	UPROPERTY(EditAnywhere, Category=Input)
+	TObjectPtr<UInputAction> _jumpAction;
+
+	UPROPERTY(EditAnywhere, Category=Input)
+	TObjectPtr<UInputAction> _moveAction;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	TObjectPtr<UInputAction> _lookAction;
+
 private:
+	void _jump();
+	void _stopJumping();
 	
+	void _move(const FInputActionValue& value);
+
+	void _look(const FInputActionValue& value);
 };
